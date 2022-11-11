1f71cd9ee54badf2461013450cdfd7554854f4a8
S133PY <Thiviyan@users.noreply.github.com>
2022-11-11 06:16:09 +0530

    Update qt5.ps1

--- qt5.ps1
+++ qt5.ps1
@@ -33,14 +33,14 @@ $mysql_lib_folder = "c:\\Programy\\MySQL\\lib"
 
 # Download Qt sources, unpack.
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::SecurityProtocol = 'Ssl3,Tls,Tls11,Tls12'
 
 Invoke-WebRequest -Uri $qt_sources_url -OutFile $qt_archive_file
 & "$tools_folder\7za.exe" x $qt_archive_file
 
 # Configure.
 mkdir $build_folder
-cd $build_folder
+Set-Location $build_folder
 
 & "$qt_src_base_folder\configure.bat" -debug-and-release -opensource -confirm-license -platform win32-msvc2017 -opengl desktop -no-iconv -no-dbus -no-icu -no-fontconfig -no-freetype -qt-harfbuzz -qt-doubleconversion -nomake examples -nomake tests -skip qt3d -skip qtactiveqt -skip qtcanvas3d -skip qtconnectivity -skip qtdatavis3d -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtvirtualkeyboard -skip qtwebview -skip qtscript -no-feature-webengine-v8-snapshot -no-feature-webengine-v8-snapshot-support -no-feature-webengine-geolocation -no-feature-webengine-pepper-plugins -no-feature-webengine-printing-and-pdf -no-feature-webengine-webchannel -no-feature-webengine-proprietary-codecs -no-feature-webengine-kerberos -no-feature-webengine-spellchecker -no-feature-webengine-webrtc -no-feature-webengine-sanitizer -no-feature-webengine-ui-delegates -no-feature-webengine-testsupport -mp -optimize-size -shared -prefix $prefix_folder -openssl-linked -I $mysql_include_folder -L $mysql_lib_folder -I $openssl_include_folder -L $openssl_libs_folder OPENSSL_LIBS="-lUser32 -lAdvapi32 -lGdi32 -llibcrypto -llibssl"
 
@@ -50,23 +50,23 @@ nmake
 nmake install
 
 # Copy qtbinpatcher, OpenSSL, MySQL.
-cp "$tools_folder\qtbinpatcher.*" "$prefix_folder\bin\"
-cp "$openssl_bin_folder\*.dll" "$prefix_folder\bin\"
-cp "$openssl_bin_folder\*.exe" "$prefix_folder\bin\"
-cp "$openssl_bin_folder\*.pdb" "$prefix_folder\bin\"
-cp "$openssl_libs_folder\*" "$prefix_folder\lib\" -Recurse
-cp "$openssl_include_folder\openssl" "$prefix_folder\include\" -Recurse
+Copy-Item "$tools_folder\qtbinpatcher.*" "$prefix_folder\bin\"
+Copy-Item "$openssl_bin_folder\*.dll" "$prefix_folder\bin\"
+Copy-Item "$openssl_bin_folder\*.exe" "$prefix_folder\bin\"
+Copy-Item "$openssl_bin_folder\*.pdb" "$prefix_folder\bin\"
+Copy-Item "$openssl_libs_folder\*" "$prefix_folder\lib\" -Recurse
+Copy-Item "$openssl_include_folder\openssl" "$prefix_folder\include\" -Recurse
 
-cp "$mysql_lib_folder\libmysql.*" "$prefix_folder\bin\"
-cp "$mysql_lib_folder\libmysql.*" "$prefix_folder\lib\"
+Copy-Item "$mysql_lib_folder\libmysql.*" "$prefix_folder\bin\"
+Copy-Item "$mysql_lib_folder\libmysql.*" "$prefix_folder\lib\"
 
 # Fixup OpenSSL DLL paths and MySQL paths.
 $openssl_libs_folder_esc = $openssl_libs_folder -replace '\\','\\'
 $mysql_lib_folder_esc = $mysql_lib_folder -replace '\\','\\'
 
-gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; (get-content $a).Replace($openssl_libs_folder_esc, '$$$$[QT_INSTALL_LIBS]\\') | set-content $a }
+Get-ChildItem -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; (get-content $a).Replace($openssl_libs_folder_esc, '$$$$[QT_INSTALL_LIBS]\\') | set-content $a }
 
-gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; (get-content $a).Replace($mysql_lib_folder_esc, '$$$$[QT_INSTALL_LIBS]\\') | set-content $a }
+Get-ChildItem -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; (get-content $a).Replace($mysql_lib_folder_esc, '$$$$[QT_INSTALL_LIBS]\\') | set-content $a }
 
 # Create final archive.
 & "$tools_folder\7za.exe" a -t7z "${prefix_base_folder}.7z" "$prefix_folder" -mmt -mx9
